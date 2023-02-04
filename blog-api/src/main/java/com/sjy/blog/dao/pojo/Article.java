9bc6e57a2f4ea8eecb1b5d64dd55ae6108b748d0
AryaErin <908159199@qq.com>
2023-02-04 16:29:35 +0800

    防止update更新时entity不需要更新的值不为null

--- blog-api/src/main/java/com/sjy/blog/dao/pojo/Article.java
+++ blog-api/src/main/java/com/sjy/blog/dao/pojo/Article.java
@@ -18,9 +18,9 @@ public class Article {
 
     private String summary;
 
-    private int commentCounts;
+    private Integer commentCounts;
 
-    private int viewCounts;
+    private Integer viewCounts;
 
     /**
      * 作者id
@@ -38,7 +38,7 @@ public class Article {
     /**
      * 置顶
      */
-    private int weight = Article_Common;
+    private Integer weight;
 
 
     /**
